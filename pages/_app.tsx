d7d1e9f7a73e7bdf1472c4a6c522a0d45fa752f9
Narek <narek07012020@gmail.com>
2022-12-28 00:52:07 +0400

    remove framer motion | remove unused imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,43 +1,14 @@
 import type { AppProps } from "next/app"
-import { motion } from "framer-motion"
 import "../styles/globals.scss"
 import Merger from "../components/Merger"
-import { useRouter } from "next/router"
 import { Analytics } from "@vercel/analytics/react"
 import "@splidejs/react-splide/css/sea-green"
 
 export default function App({ Component, pageProps }: AppProps) {
-	const { pathname } = useRouter()
-
 	return (
-		<>
-			<Merger>
-				<motion.div
-					key={pathname}
-					initial='initialState'
-					animate='animateState'
-					exit='exitState'
-					transition={{ duration: 0.45 }}
-					variants={{
-						initialState: {
-							opacity: 0,
-							visibility: "hidden",
-							clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-						},
-						animateState: {
-							opacity: 1,
-							visibility: "visible",
-							clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-						},
-						exitState: {
-							clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
-						},
-					}}
-				>
-					<Component {...pageProps} />
-				<Analytics />
-			</Merger>
-				</motion.div>
-		</>
+		<Merger>
+			<Component {...pageProps} />
+			<Analytics />
+		</Merger>
 	)
 }
