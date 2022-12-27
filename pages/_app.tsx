157c7ee834c64fd4ed3d1bcb2ea66bc6711f5910
Narek <narek07012020@gmail.com>
2022-12-28 00:48:48 +0400

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
 import type { AppProps } from "next/app"
-import { AnimatePresence, motion } from "framer-motion"
+import { motion } from "framer-motion"
 import "../styles/globals.scss"
 import Merger from "../components/Merger"
-import Head from "next/head"
 import { useRouter } from "next/router"
 import { Analytics } from "@vercel/analytics/react"
 import "@splidejs/react-splide/css/sea-green"
@@ -12,40 +11,33 @@ export default function App({ Component, pageProps }: AppProps) {
 
 	return (
 		<>
-			<Head>
-				<title>Maba Embroideries</title>
-				<link rel='icon' sizes='all' type='image/svg+xml' href='/Logo.svg' />
-			</Head>
-
 			<Merger>
-				<AnimatePresence mode='wait'>
-					<motion.div
-						key={pathname}
-						initial='initialState'
-						animate='animateState'
-						exit='exitState'
-						transition={{ duration: 0.45 }}
-						variants={{
-							initialState: {
-								opacity: 0,
-								visibility: "hidden",
-								clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-							},
-							animateState: {
-								opacity: 1,
-								visibility: "visible",
-								clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-							},
-							exitState: {
-								clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
-							},
-						}}
-					>
-						<Component {...pageProps} />
-					</motion.div>
-				</AnimatePresence>
+				<motion.div
+					key={pathname}
+					initial='initialState'
+					animate='animateState'
+					exit='exitState'
+					transition={{ duration: 0.45 }}
+					variants={{
+						initialState: {
+							opacity: 0,
+							visibility: "hidden",
+							clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+						},
+						animateState: {
+							opacity: 1,
+							visibility: "visible",
+							clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+						},
+						exitState: {
+							clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+						},
+					}}
+				>
+					<Component {...pageProps} />
 				<Analytics />
 			</Merger>
+				</motion.div>
 		</>
 	)
 }
