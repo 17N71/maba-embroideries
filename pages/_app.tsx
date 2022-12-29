40467fc8d19c1a8a17ea34e563c4c950cc124b5f
Narek <narek07012020@gmail.com>
2022-12-29 22:47:08 +0400

    font import in apptsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from "next/app"
+import "../styles/Tiempos/stylesheet.scss"
+import "../styles/poppins/stylesheet.scss"
 import "../styles/globals.scss"
 import Merger from "../components/Merger"
 import { Analytics } from "@vercel/analytics/react"
