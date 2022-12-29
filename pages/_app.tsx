507fe5b773b28349d1d4b50579d751cec8000afe
Narek <narek07012020@gmail.com>
2022-12-29 22:54:05 +0400

    remove page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,6 @@ import { Analytics } from "@vercel/analytics/react"
 import "@splidejs/react-splide/css/sea-green"
 
 export default function App({ Component, pageProps }: AppProps) {
-	const r = useRouter()
 	return (
 		<Merger>
 			<Component {...pageProps} />
